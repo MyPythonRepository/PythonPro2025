6bc1c7ca3cae6db7841b7df7523d34a5b8fc2de6
MyPythonRepository <iryna.sharova@ukr.net>
2025-06-13 14:14:43 +0300

    HT_23

--- HT_23/HT_23.1.py
+++ HT_23/HT_23.1.py
@@ -7,11 +7,26 @@
 dict1 = {"a": 1, "b": 2, "C": 80}
 
 
-def map_dict(my_dict, func1, func2):
-    return iter((func1(k), func2(v)) for k, v in my_dict.items())
+class MapDict:
+    def __init__(self, my_dict, func1, func2):
+        self._items = list(my_dict.items())
+        self.func1 = func1
+        self.func2 = func2
+        self.index = 0
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index >= len(self._items):
+            raise StopIteration
+        key, value = self._items[self.index]
+        self.index += 1
+        return (self.func1(key), self.func2(value))
 
 
-new_iter = map_dict(dict1, str.upper, lambda x: x * 20)
+new_iter = MapDict(dict1, str.upper, lambda x: x * 20)
 
 new_dict = dict(new_iter)
 print(new_dict)
